20b451de25da74f5df006448d008a9a111b4314c
Eseasky 03 <eseasky03@Eseasky-03.local>
2019-12-18 12:04:06 +0800

    新增验证码

---
+++ security-verifycode/src/main/java/com/example/security/service/MyUserDetailService.java
@@ -0,0 +1,32 @@
+package com.example.security.service;
+
+import com.example.security.bean.MyUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        MyUser user = new MyUser();
+        user.setUsername(s);
+        user.setPassword(this.passwordEncoder.encode("123456"));
+
+        System.out.println("-------------密码: " + user.getPassword());
+
+        return new User(s, user.getPassword(), user.isEnabled(),
+                user.isAccountNonExpired(),
+                user.isCredentialsNonExpired(),
+                user.isAccountNonLocked(),
+                AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+}
---
+++ security/src/main/java/com/example/security/component/MyAuthenticationSucessHandler.java
@@ -0,0 +1,4 @@
+package com.example.security.component;
+
+public class MyAuthenticationSucessHandler {
+}
